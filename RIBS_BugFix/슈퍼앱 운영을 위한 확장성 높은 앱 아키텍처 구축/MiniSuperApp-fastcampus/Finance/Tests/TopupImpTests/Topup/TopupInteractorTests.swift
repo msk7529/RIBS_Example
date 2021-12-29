b9aa387e5844751e2a5021eed84140667771cc4c
G.magpie <msk7529@naver.com>
2021-12-29 16:02:54 +0900

    ToupInterator 테스트코드 작성 마무리

--- RIBS_BugFix/슈퍼앱 운영을 위한 확장성 높은 앱 아키텍처 구축/MiniSuperApp-fastcampus/Finance/Tests/TopupImpTests/Topup/TopupInteractorTests.swift
+++ RIBS_BugFix/슈퍼앱 운영을 위한 확장성 높은 앱 아키텍처 구축/MiniSuperApp-fastcampus/Finance/Tests/TopupImpTests/Topup/TopupInteractorTests.swift
@@ -66,4 +66,97 @@ final class TopupInteractorTests: XCTestCase {
         XCTAssertEqual(router.attachAddPaymentMethodCallCount, 1)
         XCTAssertEqual(router.attachAddPaymentMethodCloseButtonType, .close)
     }
+    
+    func testDidAddCardWithCard() {
+        // given
+        let cards = [
+            PaymentMethod(id: "0", name: "Zero", digits: "0123", color: "", isPrimary: false)
+        ]
+        cardOnFileRepository.cardOnFileSubject.send(cards)
+        
+        let newCard = PaymentMethod(id: "new_card_id", name: "New Card", digits: "0000", color: "", isPrimary: false)
+        
+        // when
+        sut.activate()
+        sut.addPaymentMethodDidAddCard(paymentMethod: newCard)
+        
+        // then
+        XCTAssertEqual(router.popToRootCallCount, 1)
+        XCTAssertEqual(dependency.paymentMethodStream.value.id, "new_card_id")
+    }
+    
+    func testDidAddCardWithoutCard() {
+        // given
+        cardOnFileRepository.cardOnFileSubject.send([])
+        
+        let newCard = PaymentMethod(id: "new_card_id", name: "New Card", digits: "0000", color: "", isPrimary: false)
+        
+        // when
+        sut.activate()
+        sut.addPaymentMethodDidAddCard(paymentMethod: newCard)
+        
+        // then
+        XCTAssertEqual(router.attachEnterAmountCallCount, 1)
+        XCTAssertEqual(dependency.paymentMethodStream.value.id, "new_card_id")
+    }
+    
+    func testAddPaymentMethodDidTapCloseFromEnterAmount() {
+        // given
+        let cards = [
+            PaymentMethod(id: "0", name: "Zero", digits: "0123", color: "", isPrimary: false)
+        ]
+        cardOnFileRepository.cardOnFileSubject.send(cards)
+        
+        // when
+        sut.activate()
+        sut.addPaymentMethodDidTapClose()
+        
+        // then
+        XCTAssertEqual(router.detachAddPaymentMethodCallCount, 1)
+    }
+    
+    func testAddPaymentMethodDidTapClose() {
+        // given
+        cardOnFileRepository.cardOnFileSubject.send([])
+        
+        // when
+        sut.activate()
+        sut.addPaymentMethodDidTapClose()
+        
+        // then
+        XCTAssertEqual(router.detachAddPaymentMethodCallCount, 1)
+        XCTAssertEqual(listener.topupDidCloseCallCount, 1)
+    }
+    
+    func testDidSelectCard() {
+        //given
+        let cards = [
+            PaymentMethod(id: "0", name: "Zero", digits: "0123", color: "", isPrimary: false),
+            PaymentMethod(id: "1", name: "One", digits: "1234", color: "", isPrimary: false)
+        ]
+        cardOnFileRepository.cardOnFileSubject.send(cards)
+        
+        // when
+        sut.cardOnDidSelect(at: 0)
+        
+        // then
+        XCTAssertEqual(dependency.paymentMethodStream.value.id, "0")
+        XCTAssertEqual(router.detachCardOnFileCallCount, 1)
+    }
+    
+    func testDidSelectCardWithInvalidIndex() {
+        //given
+        let cards = [
+            PaymentMethod(id: "0", name: "Zero", digits: "0123", color: "", isPrimary: false),
+            PaymentMethod(id: "1", name: "One", digits: "1234", color: "", isPrimary: false)
+        ]
+        cardOnFileRepository.cardOnFileSubject.send(cards)
+        
+        // when
+        sut.cardOnDidSelect(at: 2)
+        
+        // then
+        XCTAssertEqual(dependency.paymentMethodStream.value.id, "")
+        XCTAssertEqual(router.detachCardOnFileCallCount, 1)
+    }
 }
